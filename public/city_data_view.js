a6b2651b473ef79487ef7809e997c0e0b8cf77ca
Suzanne <suzanne.aitchison@gmail.com>
2017-04-14 14:47:15 +0100

    clears previous city's data before repopulating

--- public/city_data_view.js
+++ public/city_data_view.js
@@ -4,10 +4,17 @@ var CityDataView = function(sectionElement){
 
 CityDataView.prototype = {
   populate: function(cityInfo){
+    this.clear()
     cityInfo.forEach(function(category){
       var pTag = document.createElement('p')
       pTag.innerText = category.name + ': ' + category.score_out_of_10
       this.sectionElement.appendChild(pTag)
     }.bind(this))
+  },
+
+  clear: function(){
+    while (this.sectionElement.hasChildNodes()){
+      this.sectionElement.removeChild(this.sectionElement.firstChild)
+    }
   }
 }
